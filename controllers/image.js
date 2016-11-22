391e6f601538dd050a37786c4558c1100798e6d3
Ivan <madritistaaa@gmail.com>
2016-11-22 10:14:39 +0200

    Add authentication for upload

--- controllers/image.js
+++ controllers/image.js
@@ -1,10 +1,18 @@
 exports.getFileUpload = (req, res) => {
+    if (!req.user) {
+        return res.redirect('/');
+    }
+
     res.render('upload', {
         title: 'File Upload'
     });
 };
 
 exports.postFileUpload = (req, res) => {
+    if (!req.user) {
+        return res.redirect('/');
+    }
+
     req.flash('success', {
         msg: 'File was uploaded successfully.'
     });
