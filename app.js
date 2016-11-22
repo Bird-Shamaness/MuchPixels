7de6ce3c744ca87033d06a4dcd653f42e271fe67
Ivan <madritistaaa@gmail.com>
2016-11-22 12:55:56 +0200

    Change port

--- app.js
+++ app.js
@@ -229,7 +229,7 @@ app.use(errorHandler());
 /**
  * Start Express server.
  */
-app.listen(app.get('port'), () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), app.get('port'), app.get('env'));
   console.log('  Press CTRL-C to stop\n');
 });
