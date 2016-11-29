0f1c0199bdcda8ba73ef55cf7de5488c6619944f
Branimir <brani_svetoslavov@abv.bg>
2016-11-29 18:20:01 +0200

    Add exceptions to upload and some styles to the upload view

--- controllers/upload-controller.js
+++ controllers/upload-controller.js
@@ -14,17 +14,34 @@ module.exports = function (data) {
     postPhotoUpload(req, res) {
       if (!req.user) {
         return res.redirect('/');
-      } 
+      } else if(!req){
+        req.flash('errors', {
+          msg: 'You have to put valid path to your photo.'
+        });
+        return res.redirect('/upload');
+      } else if(!req.file){
+        req.flash('errors', {
+          msg: 'You have to put valid path to your photo.'
+        });
+       
+        return res.redirect('/upload');
+      } else if(!req.body.title){
+        req.flash('errors', {
+          msg: 'You have to choose a title of your photo.'
+        });
+
+        return res.redirect('/upload');
+      }
 
       const photoDestination = req.file.path;
-      console.log(req.body);
+
       data.createPhoto(fs.readFileSync(photoDestination), req.file.mimetype, req.user.email, req.body.title, req.body.description)
       .then((photo) => {
         fs.unlinkSync(photoDestination);
 
         req.flash('success', {
           msg: 'Your photo was uploaded successfully.'
-        });
+        }); 
 
         res.redirect(`/photo/details/${photo._id}`);
       });
