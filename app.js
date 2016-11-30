7f09a160ed68a263f215278dab0232e76a4d5014
shopov91 <shopov91@gmail.com>
2016-11-30 12:44:01 +0200

    Serever redone in app.js

--- app.js
+++ app.js
@@ -73,6 +73,7 @@ const app = express();
 /**
  * Express configuration.
  */
+
 const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
 let users = [];
@@ -85,42 +86,7 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/public');
 });
 
-//  messenger logic
-app.get('/messenger', function(req, res) { // тука е раута
-    res.render("messenger");
-});
 
-app.use(express.static(__dirname + '/public'));
-
-io.sockets.on('connection', function(socket) {
-    connections.push(socket);
-    console.log('Connected: %s sockets connnected', connections.length);
-
-    // Disconnect
-    socket.on('disconnect', function(data) {
-        users.splice(users.indexOf(socket.username), 1);
-        updateUsernames();
-        connections.splice(connections.indexOf(socket), 1);
-        console.log('Disconnected %s sockets connected', connections.length);
-    });
-
-    // Send Message
-    socket.on('send message', function(data) {
-        io.sockets.emit('new message', { msg: data, user: socket.username });
-    });
-
-    // New User 
-    socket.on('new user', function(data, callback) {
-        callback(true);
-        socket.username = data;
-        users.push(socket.username);
-        updateUsernames();
-    });
-
-    function updateUsernames() {
-        io.sockets.emit('get users', users)
-    }
-});
 
 //app.set('port', process.env.PORT || 3000); // in conflict with messenger server.listen/ Should be removed, logic extended in server.listen
 app.set('views', path.join(__dirname, 'views'));
