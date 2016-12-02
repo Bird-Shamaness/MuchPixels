79702175a1fee95284f5cae4e81ac5db8f512a39
shopov91 <shopov91@gmail.com>
2016-12-02 18:42:59 +0200

    Added message model

---
+++ models/Message.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+  author: String,
+  content: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Message = mongoose.model('Message', messageSchema);
+
+module.exports = Message;
