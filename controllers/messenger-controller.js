2e028f1278220cea490856d9fe886ecfa07dc061
shopov91 <shopov91@gmail.com>
2016-12-04 19:50:58 +0200

    Revert "Partialy implemented data"
    This reverts commit f06335fcd172e063d6a40904d1005ac9867a8433.

--- controllers/messenger-controller.js
+++ controllers/messenger-controller.js
@@ -1,19 +1,5 @@
 module.exports = {
-  getMessenger(req, res) {
-    res.render('messenger');
-  }
+    getMessenger(req, res) {
+      res.render('messenger');
+    }
 };
-
-// module.exports = function(data) {
-//   return {
-//     getMessenger(req, res) {
-//       res.render('messenger');
-//     },
-//     insertMessage(req, res) {
-//       data.createMessage(req.params.author, req.body.content, req.date)
-//         .then((successMessage) => {
-//           res.send;
-//         });
-//     }
-//   };
-// };
