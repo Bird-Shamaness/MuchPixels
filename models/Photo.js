33bc94869351b4396e497b973ba496442f78fe29
KristiyanVachev <typhon04@gmail.com>
2016-12-04 11:24:27 +0200

    Added tags for pictures

--- models/Photo.js
+++ models/Photo.js
@@ -1,42 +1,46 @@
 const mongoose = require('mongoose');
 
 const photoSchema = new mongoose.Schema({
-  url: {
-    type: String,
-    required: true
-  },
-  author: String,
-  date: {
-    type: Date,
-    default: Date.now
-  },
-  title: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String
-  },
-  upvotes: [{
-    user: String
-  }],
-  comments: [{
-    user: {
-      type: String,
-      required: true
+    url: {
+        type: String,
+        required: true
     },
-    content: {
-      type: String,
-      required: true
-    },
+    author: String,
     date: {
-      type: Date,
-      default: Date.now
+        type: Date,
+        default: Date.now
     },
-    postId: mongoose.Schema.Types.ObjectId
-  }],
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    tags: [{
+        type: String,
+        required: true
+    }],
+    upvotes: [{
+        user: String
+    }],
+    comments: [{
+        user: {
+            type: String,
+            required: true
+        },
+        content: {
+            type: String,
+            required: true
+        },
+        date: {
+            type: Date,
+            default: Date.now
+        },
+        postId: mongoose.Schema.Types.ObjectId
+    }],
 }, {
-  timestamps: true
+    timestamps: true
 });
 
 const Photo = mongoose.model('Photo', photoSchema);
